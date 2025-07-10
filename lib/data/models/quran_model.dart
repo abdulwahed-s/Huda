f6831dfd08152c9af317846ff9e49db297ae8b0d
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-10 22:01:35 +0400

    feat: add QuranModel class for representing Quran data with JSON serialization

---
+++ lib/data/models/quran_model.dart
@@ -0,0 +1,36 @@
+class QuranModel {
+  int? number;
+  String? name;
+  String? englishName;
+  String? englishNameTranslation;
+  int? numberOfAyahs;
+  String? revelationType;
+
+  QuranModel(
+      {this.number,
+      this.name,
+      this.englishName,
+      this.englishNameTranslation,
+      this.numberOfAyahs,
+      this.revelationType});
+
+  QuranModel.fromJson(Map<String, dynamic> json) {
+    number = json['number'];
+    name = json['name'];
+    englishName = json['englishName'];
+    englishNameTranslation = json['englishNameTranslation'];
+    numberOfAyahs = json['numberOfAyahs'];
+    revelationType = json['revelationType'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['number'] = number;
+    data['name'] = name;
+    data['englishName'] = englishName;
+    data['englishNameTranslation'] = englishNameTranslation;
+    data['numberOfAyahs'] = numberOfAyahs;
+    data['revelationType'] = revelationType;
+    return data;
+  }
+}
