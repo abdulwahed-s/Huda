323e874b6b3b8cab3bfcd6932936be2d385dcdc8
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-12 19:49:18 +0400

    feat: add QuranEmptyState widget for displaying no search results

---
+++ lib/presentation/widgets/quran_empty_state.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class QuranEmptyState extends StatelessWidget {
+  const QuranEmptyState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.search_off,
+            size: 80,
+            color: Colors.grey[400],
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'No Surahs Found',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey[600],
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Try adjusting your search',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey[400],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
