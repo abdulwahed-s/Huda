ebd0ea158a2ef2ba8eb4fbea266100d7aa684ff5
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-17 21:17:43 +0400

    feat: add NextPrayerCountdown model for managing prayer countdowns

---
+++ lib/data/models/countdown_model.dart
@@ -0,0 +1,9 @@
+class NextPrayerCountdown {
+  final String prayerName;
+  final Duration duration;
+
+  const NextPrayerCountdown({
+    required this.prayerName,
+    required this.duration,
+  });
+}
