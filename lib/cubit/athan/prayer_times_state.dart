6ba01d665be4dc5e36b3628903df190ac9eb25f0
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-19 15:58:08 +0400

    feat: add PrayerTimesState classes for managing prayer times states

---
+++ lib/cubit/athan/prayer_times_state.dart
@@ -0,0 +1,18 @@
+part of 'prayer_times_cubit.dart';
+
+abstract class PrayerTimesState {}
+
+class PrayerTimesInitial extends PrayerTimesState {}
+
+class PrayerTimesLoading extends PrayerTimesState {}
+
+class PrayerTimesLoaded extends PrayerTimesState {
+  final PrayerTimes prayerTimes;
+  final List<Placemark> placemarks;
+  PrayerTimesLoaded(this.prayerTimes, this.placemarks);
+}
+
+class PrayerTimesError extends PrayerTimesState {
+  final String message;
+  PrayerTimesError(this.message);
+}
