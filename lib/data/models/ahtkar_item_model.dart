45f4b08282f9fc6f3a41130ec2dd6f30ae9691dc
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-20 21:07:47 +0400

    feat: create AthkarItem model for representing Athkar data

---
+++ lib/data/models/ahtkar_item_model.dart
@@ -0,0 +1,17 @@
+class AthkarItem {
+  final int id;
+  final String titleAr;
+  final String titleEn;
+  final String audioUrl;
+  final String textUrlAr;
+  final String textUrlEn;
+
+  AthkarItem({
+    required this.id,
+    required this.titleAr,
+    required this.titleEn,
+    required this.audioUrl,
+    required this.textUrlAr,
+    required this.textUrlEn,
+  });
+}
