47ed5a7477c25eef24c62af692f65f045a447b6d
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-21 14:23:02 +0400

    feat: add endpoints for fetching Athkar data from external API

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -2,5 +2,9 @@ class EndPoints {
   static const String baseUrl = 'https://api.alquran.cloud/v1';
   static const String edition = '/edition';
   static String surahEdition(String identifier) => '/quran/$identifier';
-  static String oneSurahEdition(String identifier,int number) => '/surah/$number/$identifier';
+  static String oneSurahEdition(String identifier, int number) =>'/surah/$number/$identifier';
+  static const String baseUrlAthkar = 'http://www.hisnmuslim.com/api';
+  static const arAthkar = "/ar/husn_ar.json";
+  static const enAthkar = "/en/husn_en.json";
+  static String athkarDetail(String id) => '/en/$id.json';
 }
