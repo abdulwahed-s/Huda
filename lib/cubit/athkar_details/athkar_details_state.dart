6bc389c34147cc4d927cf5e0ee781352dcecb8fb
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-21 22:21:32 +0400

    feat: add AthkarDetailsState classes for managing Athkar details and loading states

---
+++ lib/cubit/athkar_details/athkar_details_state.dart
@@ -0,0 +1,20 @@
+part of 'athkar_details_cubit.dart';
+
+@immutable
+sealed class AthkarDetailsState {}
+
+final class AthkarDetailsInitial extends AthkarDetailsState {}
+
+final class AthkarDetailsLoading extends AthkarDetailsState {}
+
+final class AthkarDetailsLoaded extends AthkarDetailsState {
+  final AthkarCategory athkarCategory;
+
+  AthkarDetailsLoaded(this.athkarCategory);
+}
+
+final class AthkarDetailsError extends AthkarDetailsState {
+  final String message;
+
+  AthkarDetailsError(this.message);
+}
