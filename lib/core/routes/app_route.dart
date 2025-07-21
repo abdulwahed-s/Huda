3fdc8dff3cb7b62f6005c4e8a167d633d8cd8bf6
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-21 14:36:31 +0400

    feat: add additional routes for Quran and Athkar navigation

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,3 +1,13 @@
 class AppRoute {
   static const String home = '/';
+  static const String homeQuran = '/homeQuran';
+  static const String surahDetails = '/surahDetails';
+  static const String lastReadSurah = '/lastReadSurah';
+  static const String settings = '/settings';
+  static const String bookmarks = '/bookmarks';
+  static const String widgetManagement = '/widgetManagement';
+  static const String prayerTimes = '/prayerTimes';
+  static const String notification = '/notification';
+  static const String athkar = '/athkar';
+  static const String athkarDetail = '/athkarDetail';
 }
