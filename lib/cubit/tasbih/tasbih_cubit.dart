3b841e5df300756854729db6e23ffb3525501305
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-23 20:10:28 +0400

    feat: implement TasbihCubit for managing Tasbih state and vibration

---
+++ lib/cubit/tasbih/tasbih_cubit.dart
@@ -0,0 +1,82 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:huda/core/cache/cache_helper.dart';
+import 'package:huda/core/services/service_locator.dart';
+import 'package:meta/meta.dart';
+import 'package:vibration/vibration.dart';
+
+part 'tasbih_state.dart';
+
+class TasbihCubit extends Cubit<TasbihState> {
+  TasbihCubit() : super(TasbihInitial());
+  CacheHelper cacheHelper = getIt<CacheHelper>();
+  int count = 0;
+  bool mode = true; // true for enabling vibration, false for disabling
+  String? note;
+  TextEditingController noteController = TextEditingController();
+
+  void increment() async {
+    count++;
+    toggleVibration();
+    saveTasbih();
+    emit(TasbihLoaded(count, mode, note));
+  }
+
+  void reset() {
+    count = 0;
+    saveTasbih();
+    emit(TasbihLoaded(count, mode, note));
+  }
+
+  void changeMode(bool newMode) {
+    mode = newMode;
+    emit(TasbihLoaded(count, mode, note));
+  }
+
+  void loadTasbih() {
+    emit(TasbihLoading());
+    try {
+      count = cacheHelper.getData(key: 'tasbih_count') ?? 0;
+      mode = cacheHelper.getData(key: 'tasbih_mode') ?? true;
+      note = cacheHelper.getData(key: 'tasbih_note');
+      noteController.text = note ?? '';
+      emit(TasbihLoaded(count, mode, note));
+    } catch (e) {
+      emit(TasbihError(e.toString()));
+    }
+  }
+
+  void saveTasbih() {
+    cacheHelper.saveData(key: 'tasbih_count', value: count);
+    cacheHelper.saveData(key: 'tasbih_mode', value: mode);
+  }
+
+  void toggleVibration() async {
+    if (mode) {
+      if (await Vibration.hasVibrator()) {
+        if (await Vibration.hasCustomVibrationsSupport()) {
+          if (await Vibration.hasAmplitudeControl()) {
+            Vibration.vibrate(duration: 50, amplitude: 128);
+          } else {
+            Vibration.vibrate(duration: 50);
+          }
+        } else {
+          Vibration.vibrate();
+        }
+      }
+    }
+  }
+
+  void setNote() {
+    note = noteController.text;
+    saveTasbih();
+    emit(TasbihLoaded(count, mode, note));
+  }
+
+  void clearNote() {
+    note = null;
+    noteController.clear();
+    saveTasbih();
+    emit(TasbihLoaded(count, mode, note));
+  }
+}
