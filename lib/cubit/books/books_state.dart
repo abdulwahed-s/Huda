7ed99b2c757c4b44e988dfcb059784a846c21578
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-07-28 21:22:38 +0400

    feat: implement BooksState classes for state management

---
+++ lib/cubit/books/books_state.dart
@@ -0,0 +1,26 @@
+part of 'books_cubit.dart';
+
+sealed class BooksState extends Equatable {
+  const BooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class BooksInitial extends BooksState {}
+
+final class BooksLoading extends BooksState {}
+
+final class BooksLoaded extends BooksState {
+  final BooksResponse booksResponse;
+
+  const BooksLoaded(this.booksResponse);
+}
+
+final class BooksError extends BooksState {
+  final String message;
+
+  const BooksError(this.message);
+}
+
+final class BooksOffline extends BooksState {}
