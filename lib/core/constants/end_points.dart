73625024a80960013f63ff074441c973ea1a4201
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-08-06 14:24:38 +0400

    feat: add hadith and islamhouse API endpoints to EndPoints class

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -7,4 +7,13 @@ class EndPoints {
   static const arAthkar = "/ar/husn_ar.json";
   static const enAthkar = "/en/husn_en.json";
   static String athkarDetail(String id) => '/en/$id.json';
+  static const String hadithBaseUrl = 'https://hadithapi.com/api';
+  static const String hadithBooks = '/books';
+  static String bookChapter(String bookSlug) => 'https://hadithapi.com/api/$bookSlug/chapters';
+  static const String hadithDetail = '/hadiths';
+  static const String islamhouseBaseUrl = 'https://api3.islamhouse.com/v3/paV29H2gm56kvLPy';
+  static String books(String lang,int page,String respLang) => '$islamhouseBaseUrl/main/books/$respLang/$lang/$page/25/json';
+  static String bookDetail(String lang, int bookId) => '$islamhouseBaseUrl/main/get-item/$bookId/$lang/json';
+  static String bookLanguages(int sourceId,String respLang) => '$islamhouseBaseUrl/main/get-item-translations/$sourceId/$respLang/json';
+  static String allBooksLanguages(String lang) => '$islamhouseBaseUrl/main/get-available-languages/books/$lang/json';
 }
