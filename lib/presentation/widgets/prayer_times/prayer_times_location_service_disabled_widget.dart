7be5ee5d5f31eece98b1ff1c1749424f9a6ed6c3
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-03 13:53:42 +0400

    feat: add PrayerTimesLocationServiceDisabledWidget to handle location service disabled state with responsive design and user actions for enabling location services

---
+++ lib/presentation/widgets/prayer_times/prayer_times_location_service_disabled_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:huda/cubit/athan/prayer_times_cubit.dart';
+import 'package:huda/presentation/widgets/prayer_times/action_button.dart';
+import 'package:huda/presentation/widgets/prayer_times/error_card.dart';
+
+class PrayerTimesLocationServiceDisabledWidget extends StatelessWidget {
+  const PrayerTimesLocationServiceDisabledWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ErrorCard(
+      message: "Location services are disabled. Please enable them in your device settings.",
+      actions: [
+        ActionButton(
+          text: "Try Again",
+          icon: Icons.refresh,
+          onPressed: () => context
+              .read<PrayerTimesCubit>()
+              .refreshLocationAndPrayerTimes(),
+        ),
+        SizedBox(width: 8.w),
+        ActionButton(
+          text: "Open Settings",
+          icon: Icons.settings,
+          onPressed: () => Geolocator.openLocationSettings(),
+        ),
+      ],
+    );
+  }
+}
