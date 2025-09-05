5b3959fa3f8c54bdce91555437649d0955442846
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-05 13:53:26 +0400

    feat: implement PdfPageTextCache for efficient caching and reference counting of PDF page text

---
+++ lib/presentation/widgets/pdf/pdf_page_text_cache.dart
@@ -0,0 +1,38 @@
+import 'package:huda/presentation/widgets/pdf/pdf_page_text_ref_count.dart';
+import 'package:pdfrx/pdfrx.dart';
+import 'package:synchronized/extension.dart';
+
+class PdfPageTextCache {
+  final PdfTextSearcher textSearcher;
+  PdfPageTextCache({
+    required this.textSearcher,
+  });
+
+  final _pageTextRefs = <int, PdfPageTextRefCount>{};
+
+  Future<PdfPageText> loadText(int pageNumber) async {
+    final ref = _pageTextRefs[pageNumber];
+    if (ref != null) {
+      ref.refCount++;
+      return ref.pageText;
+    }
+    return await synchronized(() async {
+      var ref = _pageTextRefs[pageNumber];
+      if (ref == null) {
+        final pageText = await textSearcher.loadText(pageNumber: pageNumber);
+        ref = _pageTextRefs[pageNumber] = PdfPageTextRefCount(pageText!);
+      }
+      ref.refCount++;
+      return ref.pageText;
+    });
+  }
+
+  void releaseText(int pageNumber) {
+    final ref = _pageTextRefs[pageNumber]!;
+    ref.refCount--;
+    if (ref.refCount == 0) {
+      _pageTextRefs.remove(pageNumber);
+    }
+  }
+}
+
