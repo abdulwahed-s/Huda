2e09af5935c5fe750f8c70cb951d2479ece7c11f
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-10 13:12:23 +0400

    feat: add LoadingState widget for displaying a shimmer loading effect with dark mode support

---
+++ lib/presentation/widgets/hadith_details/loading_state.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+
+class LoadingState extends StatelessWidget {
+  final bool isDark;
+
+  const LoadingState({super.key, required this.isDark});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0.w),
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Shimmer.fromColors(
+            baseColor: isDark ? Colors.grey[800]! : Colors.grey[300]!,
+            highlightColor: isDark ? Colors.grey[700]! : Colors.grey[100]!,
+            child: Container(
+              height: 180.h,
+              margin: EdgeInsets.only(bottom: 16.0.h),
+              decoration: BoxDecoration(
+                color: isDark ? Colors.grey[800] : Colors.grey[200],
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
