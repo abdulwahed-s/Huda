bdca26d9e3d6ddda851ce4248afefd58cfe3c33a
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-10 13:12:40 +0400

    feat: add NavigationButtons widget for navigating between hadith pages with dark mode support and responsive design

---
+++ lib/presentation/widgets/hadith_details/navigation_buttons.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:huda/cubit/hadith_details/hadith_details_cubit.dart';
+import 'package:huda/l10n/app_localizations.dart';
+import 'package:huda/presentation/widgets/hadith_details/navigation_button.dart';
+
+class NavigationButtons extends StatelessWidget {
+  final int currentPage;
+  final int lastPage;
+  final bool isDark;
+  final String chapterId;
+  final String bookId;
+
+  const NavigationButtons({
+    super.key,
+    required this.currentPage,
+    required this.lastPage,
+    required this.isDark,
+    required this.chapterId,
+    required this.bookId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: NavigationButton(
+            icon: Icons.arrow_back_ios,
+            label: AppLocalizations.of(context)!.back,
+            isEnabled: currentPage != 1,
+            onPressed: () {
+              context
+                  .read<HadithDetailsCubit>()
+                  .fetchHadithDetails(chapterId, bookId, currentPage - 1);
+            },
+            isDark: isDark,
+          ),
+        ),
+        SizedBox(width: 16.0.w),
+        Expanded(
+          child: NavigationButton(
+            icon: Icons.arrow_forward_ios,
+            label: AppLocalizations.of(context)!.next,
+            isEnabled: currentPage != lastPage,
+            onPressed: () {
+              context
+                  .read<HadithDetailsCubit>()
+                  .fetchHadithDetails(chapterId, bookId, currentPage + 1);
+            },
+            isDark: isDark,
+          ),
+        ),
+      ],
+    );
+  }
+}
