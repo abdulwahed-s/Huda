377b9fc77a242881575c558654eeaa22a1f66860
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-23 16:53:56 +0400

    feat: localize error messages and action button texts in PrayerTimesLocationServiceDisabledWidget for improved user experience

--- lib/presentation/widgets/prayer_times/prayer_times_location_service_disabled_widget.dart
+++ lib/presentation/widgets/prayer_times/prayer_times_location_service_disabled_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:huda/cubit/athan/prayer_times_cubit.dart';
+import 'package:huda/l10n/app_localizations.dart';
 import 'package:huda/presentation/widgets/prayer_times/action_button.dart';
 import 'package:huda/presentation/widgets/prayer_times/error_card.dart';
 
@@ -12,18 +13,17 @@ class PrayerTimesLocationServiceDisabledWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ErrorCard(
-      message: "Location services are disabled. Please enable them in your device settings.",
+      message: AppLocalizations.of(context)!.locationServicesDisabled,
       actions: [
         ActionButton(
-          text: "Try Again",
+          text: AppLocalizations.of(context)!.tryAgain,
           icon: Icons.refresh,
-          onPressed: () => context
-              .read<PrayerTimesCubit>()
-              .refreshLocationAndPrayerTimes(),
+          onPressed: () =>
+              context.read<PrayerTimesCubit>().refreshLocationAndPrayerTimes(),
         ),
         SizedBox(width: 8.w),
         ActionButton(
-          text: "Open Settings",
+          text: AppLocalizations.of(context)!.openSettings,
           icon: Icons.settings,
           onPressed: () => Geolocator.openLocationSettings(),
         ),
