886c39ee0e1482abaec95233fa1e734534ccc9ff
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-25 20:48:26 +0400

    refactor: simplify main application entry point by integrating bootstrap process, improving code clarity and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:huda/core/cache/cache_helper.dart';
-import 'package:huda/core/routes/page_router.dart';
-import 'package:huda/core/services/service_locator.dart';
+import 'package:huda/core/bootstrap/bootstrap.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  setupServiceLocator();
-  await getIt<CacheHelper>().init();
-  runApp(MyApp(
-    pageRouter: PageRouter(),
-  ));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.pageRouter});
-  final PageRouter pageRouter;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: pageRouter.generateRoute,
-    );
-  }
+  await bootstrap();
 }
