b78b36e0181dc14a6d2b7b0979b730c6e252433f
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-29 20:07:40 +0400

    feat: implement offline book detail fetching in BookDetailCubit for improved offline support

--- lib/cubit/book_detail/book_detail_cubit.dart
+++ lib/cubit/book_detail/book_detail_cubit.dart
@@ -3,7 +3,9 @@ import 'package:equatable/equatable.dart';
 import 'package:huda/core/connection/network_info.dart';
 import 'package:huda/data/api/books_detail_services.dart';
 import 'package:huda/data/models/books_detail_model.dart';
+import 'package:huda/data/models/offline_book_model.dart';
 import 'package:huda/data/repository/book_detail_repository.dart';
+import 'package:huda/data/services/offline_books_service.dart';
 
 part 'book_detail_state.dart';
 
@@ -11,6 +13,7 @@ class BookDetailCubit extends Cubit<BookDetailState> {
   BookDetailCubit() : super(BookDetailInitial());
   BookDetailRepository bookDetailRepository =
       BookDetailRepository(booksDetailServices: BooksDetailServices());
+  OfflineBooksService offlineBooksService = OfflineBooksService();
 
   Future<void> fetchBookDetail(int bookId, String language) async {
     if (await NetworkInfo.checkInternetConnectivity()) {
@@ -23,6 +26,21 @@ class BookDetailCubit extends Cubit<BookDetailState> {
         emit(BookDetailError(e.toString()));
       }
     } else {
+      // When offline, try to get the book from local storage
+      await fetchOfflineBookDetail(bookId);
+    }
+  }
+
+  Future<void> fetchOfflineBookDetail(int bookId) async {
+    emit(BookDetailLoading());
+    try {
+      final offlineBook = await offlineBooksService.getBook(bookId);
+      if (offlineBook != null) {
+        emit(BookDetailOfflineLoaded(offlineBook));
+      } else {
+        emit(BookDetailOffline());
+      }
+    } catch (e) {
       emit(BookDetailOffline());
     }
   }
