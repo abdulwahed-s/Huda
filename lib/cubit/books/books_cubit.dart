379768d4a0fd8195fb85d25a6a8fe7c521223e44
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-09-29 20:08:21 +0400

    feat: enhance BooksCubit to support offline book fetching and include response language parameter in fetchBooks method

--- lib/cubit/books/books_cubit.dart
+++ lib/cubit/books/books_cubit.dart
@@ -3,7 +3,9 @@ import 'package:equatable/equatable.dart';
 import 'package:huda/core/connection/network_info.dart';
 import 'package:huda/data/api/books_services.dart';
 import 'package:huda/data/models/books_response.dart';
+import 'package:huda/data/models/offline_book_model.dart';
 import 'package:huda/data/repository/books_repository.dart';
+import 'package:huda/data/services/offline_books_service.dart';
 
 part 'books_state.dart';
 
@@ -12,18 +14,40 @@ class BooksCubit extends Cubit<BooksState> {
 
   BooksRepository booksRepository =
       BooksRepository(booksServices: BooksServices());
+  OfflineBooksService offlineBooksService = OfflineBooksService();
 
-  Future<void> fetchBooks(String lang, int pageNumber) async {
+  Future<void> fetchBooks(String lang, int pageNumber, String respLang) async {
     if (await NetworkInfo.checkInternetConnectivity()) {
       emit(BooksLoading());
       try {
-        final booksResponse =
-            await booksRepository.getAllBooks(lang, pageNumber);
+        final BooksResponse booksResponse =
+            await booksRepository.getAllBooks(lang, pageNumber, respLang);
         emit(BooksLoaded(booksResponse));
       } catch (e) {
         emit(BooksError(e.toString()));
       }
     } else {
+      await fetchOfflineBooks(lang);
+    }
+  }
+
+  Future<void> fetchOfflineBooks(String lang) async {
+    emit(BooksOfflineLoading());
+    try {
+      List<OfflineBookModel> offlineBooks;
+
+      if (lang == 'showall') {
+        offlineBooks = await offlineBooksService.getAllBooks();
+      } else {
+        offlineBooks = await offlineBooksService.getBooksByLanguage(lang);
+      }
+
+      if (offlineBooks.isEmpty) {
+        emit(BooksOfflineEmpty());
+      } else {
+        emit(BooksOfflineLoaded(offlineBooks));
+      }
+    } catch (e) {
       emit(BooksOffline());
     }
   }
