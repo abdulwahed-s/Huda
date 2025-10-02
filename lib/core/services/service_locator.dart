17ebb97fcaab5d46e460782f50a9a4110d6184d8
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-10-03 03:38:00 +0400

    feat: add Service Locator for dependency management, registering core services including CacheHelper, Dio, DownloadService, ReadingPositionService, BookmarkService, PrayerCountdownService, PersistentPrayerCountdownService, and Upgrader

---
+++ lib/core/services/service_locator.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:huda/core/cache/cache_helper.dart';
+import 'package:huda/core/services/download_service.dart';
+import 'package:huda/core/services/reading_position_service.dart';
+import 'package:huda/core/services/bookmark_service.dart';
+import 'package:huda/core/services/prayer_countdown_service.dart';
+import 'package:huda/core/services/persistent_prayer_countdown_service.dart';
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:upgrader/upgrader.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<CacheHelper>(CacheHelper());
+  getIt.registerSingleton<Dio>(Dio());
+  getIt.registerSingleton<DownloadService>(DownloadService());
+  getIt.registerSingleton<ReadingPositionService>(ReadingPositionService());
+  getIt.registerSingleton<BookmarkService>(
+      BookmarkService(cacheHelper: getIt<CacheHelper>()));
+  getIt.registerSingleton<PrayerCountdownService>(PrayerCountdownService());
+  getIt.registerSingleton<PersistentPrayerCountdownService>(
+      PersistentPrayerCountdownService());
+  getIt.registerSingleton<Upgrader>(
+      Upgrader(languageCode: PlatformDispatcher.instance.locale.languageCode));
+}
