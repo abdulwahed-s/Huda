6800c6e1b394ddd953933d6af1a33d5be738bb86
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-11-10 12:01:21 +0400

    refactor: update ChaptersList to sort chapters and include bookName parameter to handle new api data

--- lib/presentation/widgets/hadith_chapters/chapters_list.dart
+++ lib/presentation/widgets/hadith_chapters/chapters_list.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:huda/core/routes/app_route.dart';
 import 'package:huda/cubit/localization/localization_cubit.dart';
+import 'package:huda/data/models/book_chapters_model.dart';
 import 'package:huda/presentation/widgets/hadith_chapters/chapter_card.dart';
 
 class ChaptersList extends StatelessWidget {
-  final List<dynamic> chapters;
+  final List<Data> chapters;
+  final String bookName;
   final bool isDark;
 
   const ChaptersList({
     super.key,
     required this.chapters,
     required this.isDark,
+    required this.bookName,
   });
 
   @override
@@ -19,12 +22,40 @@ class ChaptersList extends StatelessWidget {
     final currentLanguageCode =
         context.read<LocalizationCubit>().state.locale.languageCode;
 
+    final sortedChapters = List<Data>.from(chapters)
+      ..sort((a, b) {
+        final aBookNumber = a.bookNumber!;
+        final bBookNumber = b.bookNumber!;
+
+        if (aBookNumber == "introduction" && bBookNumber == "introduction") {
+          return 0;
+        } else if (aBookNumber == "introduction") {
+          return -1;
+        } else if (bBookNumber == "introduction") {
+          return 1;
+        } else {
+          final aMatch = RegExp(r'^(\d+)([a-zA-Z]*)$').firstMatch(aBookNumber);
+          final bMatch = RegExp(r'^(\d+)([a-zA-Z]*)$').firstMatch(bBookNumber);
+
+          final aNum = int.tryParse(aMatch?.group(1) ?? aBookNumber) ?? 0;
+          final bNum = int.tryParse(bMatch?.group(1) ?? bBookNumber) ?? 0;
+
+          if (aNum != bNum) {
+            return aNum.compareTo(bNum);
+          }
+
+          final aSuffix = aMatch?.group(2) ?? '';
+          final bSuffix = bMatch?.group(2) ?? '';
+          return aSuffix.compareTo(bSuffix);
+        }
+      });
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: ListView.builder(
-        itemCount: chapters.length,
+        itemCount: sortedChapters.length,
         itemBuilder: (context, index) {
-          final chapter = chapters[index];
+          final chapter = sortedChapters[index];
           return ChapterCard(
             chapter: chapter,
             isDark: isDark,
@@ -34,13 +65,11 @@ class ChaptersList extends StatelessWidget {
                 context,
                 AppRoute.hadithDetails,
                 arguments: {
-                  'chapterId': chapter.chapterNumber!.toString(),
-                  'bookId': chapter.bookSlug!.toString(),
-                  'chapterName': currentLanguageCode == "ur"
-                      ? chapter.chapterUrdu!.toString()
-                      : currentLanguageCode == "ar"
-                          ? chapter.chapterArabic!.toString()
-                          : chapter.chapterEnglish!.toString(),
+                  'chapterNumber': chapter.bookNumber!,
+                  'bookName': bookName,
+                  'chapterName': currentLanguageCode == "ar"
+                      ? chapter.book![1].name!.toString()
+                      : chapter.book![0].name.toString(),
                 },
               );
             },
