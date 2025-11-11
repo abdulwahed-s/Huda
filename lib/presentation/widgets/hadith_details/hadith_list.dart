d02759358ff93185a1ff79f6cab6a338f1eadcf5
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-11-11 21:59:50 +0400

    refactor: update HadithList to use HadithDetailsModel and implement EmptyHadithState for improved handling of empty data scenarios

--- lib/presentation/widgets/hadith_details/hadith_list.dart
+++ lib/presentation/widgets/hadith_details/hadith_list.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:huda/data/models/hadith_details_model.dart';
+import 'package:huda/presentation/widgets/hadith_details/empty_hadith_state.dart';
 import 'package:huda/presentation/widgets/hadith_details/hadith_card.dart';
 import 'package:huda/presentation/widgets/hadith_details/pagination_controls.dart';
 
 class HadithList extends StatelessWidget {
-  final dynamic hadithDetail;
+  final HadithDetailsModel hadithDetail;
   final bool isDark;
-  final String chapterId;
-  final String bookId;
+  final String chapterNumber;
+  final String bookName;
   final String chapterName;
 
   const HadithList({
     super.key,
     required this.hadithDetail,
     required this.isDark,
-    required this.chapterId,
-    required this.bookId,
+    required this.chapterNumber,
+    required this.bookName,
     required this.chapterName,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16.0.w),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: hadithDetail.hadiths!.data!.length,
-              itemBuilder: (context, index) {
-                final hadith = hadithDetail.hadiths!.data![index];
-                return HadithCard(
-                  hadith: hadith,
+    return hadithDetail.data!.isEmpty
+        ? EmptyHadithState(isDark: isDark)
+        : Padding(
+            padding: EdgeInsets.all(16.0.w),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: hadithDetail.data!.length,
+                    itemBuilder: (context, index) {
+                      final hadith = hadithDetail.data![index];
+                      return HadithCard(
+                        hadith: hadith,
+                        isDark: isDark,
+                        chapterName: chapterName,
+                      );
+                    },
+                  ),
+                ),
+                PaginationControls(
+                  currentPage: hadithDetail.previous == null
+                      ? 1
+                      : hadithDetail.previous! + 1,
+                  lastPage: hadithDetail.total! > hadithDetail.limit!
+                      ? (hadithDetail.total! / hadithDetail.limit!).ceil()
+                      : 1,
                   isDark: isDark,
-                  chapterName: chapterName,
-                );
-              },
+                  chapterNumber: chapterNumber,
+                  bookName: bookName,
+                ),
+              ],
             ),
-          ),
-          PaginationControls(
-            currentPage: hadithDetail.hadiths!.currentPage!,
-            lastPage: hadithDetail.hadiths!.lastPage!,
-            isDark: isDark,
-            chapterId: chapterId,
-            bookId: bookId,
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
