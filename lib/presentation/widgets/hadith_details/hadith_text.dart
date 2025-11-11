457c7e36994f0959b24988db0039789a8e30a886
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-11-11 21:58:33 +0400

    refactor: enhance HadithText widget to render HTML content with custom styling for improved display of hadith text

--- lib/presentation/widgets/hadith_details/hadith_text.dart
+++ lib/presentation/widgets/hadith_details/hadith_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class HadithText extends StatelessWidget {
   final String text;
@@ -13,21 +14,42 @@ class HadithText extends StatelessWidget {
     required this.currentLanguageCode,
   });
 
+  String _convertToHtml(String text) {
+    String converted = text
+        .replaceAll('[prematn]', '<span class="prematn">')
+        .replaceAll('[/prematn]', '</span>')
+        .replaceAll('[matn]', '<span class="matn">')
+        .replaceAll('[/matn]', '</span>');
+
+    converted = converted.replaceAllMapped(
+      RegExp(r'\[narrator id="([^"]*)" tooltip="([^"]*)"\]'),
+      (match) => '<span class="narrator" title="${match.group(2)}">',
+    );
+    converted = converted.replaceAll('[/narrator]', '</span>');
+
+    return converted;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20.0.w),
-      child: Text(
-        text,
-        style: TextStyle(
-          fontSize: 16.0.sp,
+    return Html(
+      data: _convertToHtml(text),
+      style: {
+        "*": Style(
+          fontSize: FontSize(16.0.sp),
           fontFamily: "Amiri",
-          height: 1.6,
+          lineHeight: LineHeight.number(1.6),
           color: isDark ? Colors.white.withValues(alpha: 0.87) : Colors.black87,
+          textAlign:
+              currentLanguageCode == "ar" ? TextAlign.right : TextAlign.left,
         ),
-        textAlign:
-            currentLanguageCode == "ar" ? TextAlign.right : TextAlign.left,
-      ),
+        ".matn": Style(
+          fontWeight: FontWeight.bold,
+        ),
+        ".narrator": Style(
+          color: isDark ? Colors.blue.shade300 : Colors.blue.shade700,
+        ),
+      },
     );
   }
 }
