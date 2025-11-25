c00fcba008bf3d6dedee8001ef40eafa8f382643
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-11-26 00:52:01 +0400

    feat: Enhance NextPrayerCountdown model with isPastPrayer and secondsPassed fields

--- lib/data/models/countdown_model.dart
+++ lib/data/models/countdown_model.dart
@@ -1,9 +1,13 @@
 class NextPrayerCountdown {
   final String prayerName;
   final Duration duration;
+  final bool isPastPrayer;
+  final int secondsPassed;
 
   const NextPrayerCountdown({
     required this.prayerName,
     required this.duration,
+    this.isPastPrayer = false,
+    this.secondsPassed = 0,
   });
 }
