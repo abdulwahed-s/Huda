4140ed712c30a1b817b476e0274a5af1449f566b
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-11-29 20:29:37 +0400

    chore: Remove unused SystemChrome orientation settings from bootstrap

--- lib/core/bootstrap/bootstrap.dart
+++ lib/core/bootstrap/bootstrap.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/services.dart';
 import 'package:huda/core/config/service_initializer.dart';
 import 'package:huda/presentation/screens/app.dart';
 import 'package:huda/firebase_options.dart';
@@ -8,13 +7,6 @@ import 'package:huda/presentation/screens/error.dart';
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
   // Initialize critical services and Firebase in parallel
   await Future.wait([
