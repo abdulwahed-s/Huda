3c1336a1400fccbe4604bde81c3927ac99ebe232
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-02 22:13:21 +0400

    fix: Correct endpoint URL construction in EndPoints class for proper API path formatting

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -9,7 +9,7 @@ class EndPoints {
   static String athkarDetail(String id) => '/en/$id.json';
   static const String hadithBaseUrl = 'https://api.sunnah.com/v1';
   static const String hadithBooks = '/collections';
-  static String bookChapter(String bookSlug) => '/$hadithBooks/$bookSlug/books';
+  static String bookChapter(String bookSlug) => '$hadithBooks/$bookSlug/books';
   static String hadithDetail(String bookName, String chapterNumber) => '$hadithBooks/$bookName/books/$chapterNumber/hadiths';
   static const String islamhouseBaseUrl = 'https://api3.islamhouse.com/v3/paV29H2gm56kvLPy';
   static String books(String lang,int page,String respLang) => '$islamhouseBaseUrl/main/books/$respLang/$lang/$page/25/json';
