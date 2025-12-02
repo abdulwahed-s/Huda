3ab972bd695cfbf65537d8155b60caa5c2c6ae3d
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-02 22:16:14 +0400

    feat: Implement CORS proxy handling for web requests in HadithServices to ensure proper API access across platforms

--- lib/data/api/hadith_services.dart
+++ lib/data/api/hadith_services.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:huda/core/class/dio_errors.dart';
 import 'package:huda/core/constants/end_points.dart';
 import 'package:dio/dio.dart';
@@ -10,23 +11,42 @@ class HadithServices {
         baseUrl: EndPoints.hadithBaseUrl,
         connectTimeout: const Duration(seconds: 30),
         receiveTimeout: const Duration(seconds: 30),
-        headers: {
-          "X-API-Key": apiKey,
-        });
+        headers: {"X-API-Key": apiKey});
     dio = Dio(options);
   }
 
   Future<Map<String, dynamic>> getAllBooks() async {
     try {
-      final Response response = await dio.get(
-        EndPoints.hadithBooks,
-      );
+      if (kIsWeb) {
+        final fullUrl = '${EndPoints.hadithBaseUrl}${EndPoints.hadithBooks}';
+        final proxyUrl =
+            'https://corsproxy.io/?${Uri.encodeComponent(fullUrl)}';
 
-      if (response.statusCode != 200) {
-        throw DioException(requestOptions: RequestOptions());
+        final Response response = await dio.get(
+          proxyUrl,
+          options: Options(
+            headers: {
+              "X-API-Key": apiKey,
+              "Origin": "http://localhost:8080",
+            },
+            extra: {'baseUrl': ''},
+          ),
+        );
+
+        if (response.statusCode != 200) {
+          throw DioException(requestOptions: RequestOptions());
+        }
+
+        return response.data;
+      } else {
+        final Response response = await dio.get(EndPoints.hadithBooks);
+
+        if (response.statusCode != 200) {
+          throw DioException(requestOptions: RequestOptions());
+        }
+
+        return response.data;
       }
-
-      return response.data;
     } on DioException catch (e) {
       throw Exception(getDioErrorMessage(e));
     }
