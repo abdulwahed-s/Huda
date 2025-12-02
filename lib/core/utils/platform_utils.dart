8ecb0bb2125f668e93ad2ddc961c81cb9ef8c4a2
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-02 22:14:15 +0400

    feat: Update platform detection logic in PlatformUtils to exclude web, ensuring accurate mobile and desktop checks

--- lib/core/utils/platform_utils.dart
+++ lib/core/utils/platform_utils.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class PlatformUtils {
-  static bool get isMobile => Platform.isAndroid || Platform.isIOS;
+  static bool get isMobile => !kIsWeb && (Platform.isAndroid || Platform.isIOS);
   static bool get isDesktop =>
-      Platform.isWindows || Platform.isMacOS || Platform.isLinux;
-  static bool get isAndroid => Platform.isAndroid;
-  static bool get isIOS => Platform.isIOS;
+      !kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isLinux);
+  static bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  static bool get isIOS => !kIsWeb && Platform.isIOS;
 }
