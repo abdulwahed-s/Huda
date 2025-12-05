40b4146b0d94f8f58edb30b9c135b2b750c80379
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-05 18:59:46 +0400

    feat: Add support for notification arguments in Dart entrypoint for enhanced notification handling

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -18,6 +18,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
+  project.set_dart_entrypoint_arguments({"--enable-notifications"});
 
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
