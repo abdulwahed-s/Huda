8fa089d001feaeb2e1eca7e3ce5104df2ce3deaa
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-07 15:06:31 +0400

    feat: Update NominatimService to utilize Google Maps API for reverse geocoding, incorporating the necessary API key for enhanced location accuracy

--- lib/data/api/nominatim_service.dart
+++ lib/data/api/nominatim_service.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:huda/core/class/dio_errors.dart';
 import 'package:huda/core/constants/end_points.dart';
+import 'package:huda/core/keys/hadith_key.dart';
 import 'package:huda/data/models/placemark_model.dart';
 
 class NominatimService {
@@ -8,14 +9,15 @@ class NominatimService {
 
   NominatimService()
       : dio = Dio(BaseOptions(
-          baseUrl: EndPoints.nominatimBaseUrl,
+          baseUrl: EndPoints.googleMapsBaseUrl,
           connectTimeout: const Duration(seconds: 30),
           receiveTimeout: const Duration(seconds: 30),
         ));
 
   Future<PlacemarkModel> getPlacemark(double lat, double lon) async {
     try {
-      final response = await dio.get(EndPoints.reverseGeocoding(lat, lon));
+      final response = await dio.get(
+          EndPoints.googleMapsReverseGeocoding(lat, lon, googleMapsApiKey));
       return PlacemarkModel.fromJson(response.data);
     } on DioException catch (e) {
       throw getDioErrorMessage(e);
