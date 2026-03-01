781659c840e78109cd34481304751bb79581eeba
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-03-01 08:09:34 +0400

    feat: Integrate MiqaatLockMethodHandler and register background tasks for improved notification handling

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,24 @@
 import Flutter
 import UIKit
+import UserNotifications
+import alarm
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  private var miqaatLockHandler: MiqaatLockMethodHandler?
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if let controller = window?.rootViewController as? FlutterViewController {
+      miqaatLockHandler = MiqaatLockMethodHandler(messenger: controller.binaryMessenger)
+    }
+    
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     }
+    SwiftAlarmPlugin.registerBackgroundTasks()
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
