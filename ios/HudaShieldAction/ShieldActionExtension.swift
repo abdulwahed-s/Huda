e4a55cfd8efbe696c80bfc43f7783980e8c8728a
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-03-01 08:08:58 +0400

    feat: Implement ShieldActionExtension to handle various shield actions and provide appropriate responses

---
+++ ios/HudaShieldAction/ShieldActionExtension.swift
@@ -0,0 +1,29 @@
+import ManagedSettings
+import ManagedSettingsUI
+import Foundation
+
+class ShieldActionExtension: ShieldActionDelegate {
+    
+    override func handle(action: ShieldAction, for application: ApplicationToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        handleAction(action, completionHandler: completionHandler)
+    }
+    
+    override func handle(action: ShieldAction, for webDomain: WebDomainToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        handleAction(action, completionHandler: completionHandler)
+    }
+    
+    override func handle(action: ShieldAction, for category: ActivityCategoryToken, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        handleAction(action, completionHandler: completionHandler)
+    }
+    
+    private func handleAction(_ action: ShieldAction, completionHandler: @escaping (ShieldActionResponse) -> Void) {
+        switch action {
+        case .primaryButtonPressed:
+            completionHandler(.close)
+        case .secondaryButtonPressed:
+            completionHandler(.close)
+        @unknown default:
+            completionHandler(.close)
+        }
+    }
+}
