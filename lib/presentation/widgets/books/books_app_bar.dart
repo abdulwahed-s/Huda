b956804bd29564f63748525f44a1942e85eecc7b
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-03-06 10:40:50 +0400

    refactor: Replace AppBar with SliverAppBar in BooksAppBar widget for enhanced scrolling effects and improved layout

--- lib/presentation/widgets/books/books_app_bar.dart
+++ lib/presentation/widgets/books/books_app_bar.dart
@@ -3,45 +3,66 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:huda/core/theme/theme_extension.dart';
 import 'package:huda/l10n/app_localizations.dart';
 
-class BooksAppBar extends StatelessWidget implements PreferredSizeWidget {
+class BooksAppBar extends StatelessWidget {
   final bool isDark;
 
   const BooksAppBar({super.key, required this.isDark});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
+    return SliverAppBar(
       elevation: 0,
       backgroundColor: isDark ? Colors.grey[900] : Colors.grey[50],
       iconTheme: IconThemeData(
         color: isDark ? Colors.white : Colors.black,
       ),
       surfaceTintColor: Colors.transparent,
-      title: Text(
-        AppLocalizations.of(context)!.library,
-        style: TextStyle(
-          fontSize: 24.sp,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Amiri',
-          color: isDark ? context.darkText : context.lightText,
+      pinned: true,
+      floating: false,
+      expandedHeight: 140.h,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: EdgeInsetsDirectional.only(
+          start: 50.w,
+          bottom: 5.h,
         ),
-      ),
-      actions: [
-        Container(
-          margin: EdgeInsets.only(right: 16.w, left: 16.w),
-          child: CircleAvatar(
-            backgroundColor: context.primaryExtraLightColor,
-            child: Icon(
-              Icons.menu_book_rounded,
-              color: context.primaryColor,
-              size: 20.sp,
-            ),
+        title: Text(
+          AppLocalizations.of(context)!.library,
+          style: TextStyle(
+            fontSize: 24.sp,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Amiri',
+            color: isDark ? context.darkText : context.lightText,
           ),
         ),
-      ],
+        background: Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    isDark ? context.darkCardBackground : Colors.white,
+                    isDark ? Colors.grey[900]! : Colors.grey[50]!,
+                  ],
+                ),
+              ),
+            ),
+            Positioned.directional(
+              textDirection: Directionality.of(context),
+              end: -30.w,
+              top: -10.h,
+              child: Icon(
+                Icons.menu_book_rounded,
+                size: 140.sp,
+                color: (isDark ? Colors.white : Colors.black)
+                    .withValues(alpha: 0.03),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
