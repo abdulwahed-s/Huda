c077c13e50bbbc9a2121bb2caac365b706cc8398
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-04-21 12:59:43 +0400

    feat: Improve responsiveness of OnboardingIndicator by integrating context-aware dimensions for margin, height, and width, enhancing user experience across different device sizes

--- lib/presentation/widgets/onboarding/onboarding_indicator.dart
+++ lib/presentation/widgets/onboarding/onboarding_indicator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:huda/core/utils/responsive_utils.dart';
 import 'package:huda/data/models/onboarding_data.dart';
 
 class OnboardingIndicator extends StatelessWidget {
@@ -20,9 +21,14 @@ class OnboardingIndicator extends StatelessWidget {
       builder: (context, value, child) {
         return AnimatedContainer(
           duration: const Duration(milliseconds: 300),
-          margin: EdgeInsets.symmetric(horizontal: 6.w),
-          height: 8.h,
-          width: isActive ? 32.w : 8.w,
+          margin: EdgeInsets.symmetric(
+            horizontal:
+                context.responsive(mobile: 6.w, tablet: 4.0, desktop: 4.0),
+          ),
+          height: context.responsive(mobile: 8.h, tablet: 8.0, desktop: 8.0),
+          width: isActive
+              ? context.responsive(mobile: 32.w, tablet: 28.0, desktop: 28.0)
+              : context.responsive(mobile: 8.w, tablet: 8.0, desktop: 8.0),
           decoration: BoxDecoration(
             gradient: isActive
                 ? LinearGradient(
