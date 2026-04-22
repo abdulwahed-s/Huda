56e84b7f8b4db8bc633bf7483add15cd78a461a6
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-04-23 01:25:15 +0400

    feat: Integrate Just Audio packages and initialize audio background services for enhanced media playback functionality

--- lib/core/bootstrap/bootstrap.dart
+++ lib/core/bootstrap/bootstrap.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 import 'package:huda/core/config/service_initializer.dart';
 import 'package:huda/core/services/quick_actions_service.dart';
 import 'package:huda/core/services/service_locator.dart';
 import 'package:huda/presentation/screens/app.dart';
 import 'package:huda/firebase_options.dart';
+import 'package:huda/core/services/qcf_font_service.dart';
 import 'package:huda/presentation/screens/error.dart';
 import 'package:alarm/alarm.dart';
 import 'package:huda/core/services/sahur_alarm_helper.dart';
@@ -12,10 +15,23 @@ import 'package:huda/core/services/sahur_alarm_helper.dart';
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  JustAudioMediaKit.ensureInitialized(
+    windows: true,
+    linux: true,
+    macOS: false,
+    iOS: false,
+    android: false,
+  );
+
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.huda.audio',
+    androidNotificationChannelName: 'Quran Player',
+    androidNotificationOngoing: true,
+  );
+
   await Alarm.init();
   SahurAlarmHelper.initListeners();
 
-  // Initialize critical services and Firebase in parallel
   await Future.wait([
     initializeCriticalServices(),
     Firebase.initializeApp(
@@ -26,6 +42,9 @@ Future<void> bootstrap() async {
   initializeNonCriticalServicesAsync();
 
   await getIt.allReady();
+
+  getIt<QcfFontService>(instanceName: 'qcf4').init();
+  getIt<QcfFontService>(instanceName: 'tajweed').init();
 
   setCustomErrorWidget();
 
