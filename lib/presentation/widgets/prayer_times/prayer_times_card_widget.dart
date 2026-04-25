e0aed7f3d0192ec7bb85f2ed3b88d73767bfb7fc
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-04-25 13:07:45 +0400

    feat: Integrate prayer time adjustment functionality in PrayerTimesCardWidget for dynamic offset display and user interaction

--- lib/presentation/widgets/prayer_times/prayer_times_card_widget.dart
+++ lib/presentation/widgets/prayer_times/prayer_times_card_widget.dart
@@ -4,6 +4,7 @@ import 'package:huda/core/theme/theme_extension.dart';
 import 'package:huda/cubit/athan/prayer_times_cubit.dart';
 import 'package:huda/l10n/app_localizations.dart';
 import 'package:huda/presentation/widgets/prayer_times/prayer_divider.dart';
+import 'package:huda/presentation/widgets/prayer_times/prayer_time_adjustment_bottom_sheet.dart';
 import 'package:huda/presentation/widgets/prayer_times/prayer_time_row.dart';
 import 'package:intl/intl.dart';
 
@@ -49,13 +50,30 @@ class PrayerTimesCardWidget extends StatelessWidget {
                   size: 20.sp,
                 ),
                 SizedBox(width: 10.w),
-                Text(
-                  AppLocalizations.of(context)!.prayerTimes,
-                  style: TextStyle(
-                    fontSize: 17.sp,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "Amiri",
-                    color: Colors.white,
+                Expanded(
+                  child: Text(
+                    AppLocalizations.of(context)!.prayerTimes,
+                    style: TextStyle(
+                      fontSize: 17.sp,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Amiri",
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => showPrayerTimeAdjustmentSheet(context),
+                  child: Container(
+                    padding: EdgeInsets.all(6.w),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withValues(alpha: 0.15),
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                    child: Icon(
+                      Icons.tune,
+                      color: Colors.white,
+                      size: 18.sp,
+                    ),
                   ),
                 ),
               ],
@@ -63,31 +81,43 @@ class PrayerTimesCardWidget extends StatelessWidget {
             SizedBox(height: 16.h),
             PrayerTimeRow(
               prayerName: AppLocalizations.of(context)!.fajr,
-              time: DateFormat.jm(locale).format(times.fajr),
+              time: DateFormat.jm(locale).format(times.fajr
+                  .add(Duration(minutes: state.offsets['fajr'] ?? 0))),
               icon: Icons.wb_twilight,
             ),
             const PrayerDivider(),
             PrayerTimeRow(
+              prayerName: AppLocalizations.of(context)!.sunrise,
+              time: DateFormat.jm(locale).format(times.sunrise
+                  .add(Duration(minutes: state.offsets['sunrise'] ?? 0))),
+              icon: Icons.wb_sunny_rounded,
+            ),
+            const PrayerDivider(),
+            PrayerTimeRow(
               prayerName: AppLocalizations.of(context)!.dhuhr,
-              time: DateFormat.jm(locale).format(times.dhuhr),
+              time: DateFormat.jm(locale).format(times.dhuhr
+                  .add(Duration(minutes: state.offsets['dhuhr'] ?? 0))),
               icon: Icons.wb_sunny,
             ),
             const PrayerDivider(),
             PrayerTimeRow(
               prayerName: AppLocalizations.of(context)!.asr,
-              time: DateFormat.jm(locale).format(times.asr),
+              time: DateFormat.jm(locale).format(
+                  times.asr.add(Duration(minutes: state.offsets['asr'] ?? 0))),
               icon: Icons.wb_sunny_outlined,
             ),
             const PrayerDivider(),
             PrayerTimeRow(
               prayerName: AppLocalizations.of(context)!.maghrib,
-              time: DateFormat.jm(locale).format(times.maghrib),
+              time: DateFormat.jm(locale).format(times.maghrib
+                  .add(Duration(minutes: state.offsets['maghrib'] ?? 0))),
               icon: Icons.wb_twilight,
             ),
             const PrayerDivider(),
             PrayerTimeRow(
               prayerName: AppLocalizations.of(context)!.isha,
-              time: DateFormat.jm(locale).format(times.isha),
+              time: DateFormat.jm(locale).format(times.isha
+                  .add(Duration(minutes: state.offsets['isha'] ?? 0))),
               icon: Icons.nights_stay,
             ),
           ],
