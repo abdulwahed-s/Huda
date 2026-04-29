85ba0a2d5bcf3f0612fe72972eeba204b33170b7
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-04-29 19:17:51 +0400

    feat: Add KhatmaCard widget for displaying Khatma content with responsive design and theme support

---
+++ lib/presentation/widgets/khatma/khatma_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class KhatmaCard extends StatelessWidget {
+  final Widget child;
+
+  const KhatmaCard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return Container(
+      margin: EdgeInsets.fromLTRB(16.w, 10.h, 16.w, 0),
+      decoration: BoxDecoration(
+        color: isDark ? const Color(0xFF1E1E1E) : const Color(0xFFFFF9F2),
+        borderRadius: BorderRadius.circular(18.r),
+        border: Border.all(
+          color:
+              isDark ? Colors.white10 : Colors.black.withValues(alpha: 0.06),
+        ),
+        boxShadow: isDark
+            ? []
+            : [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.05),
+                  blurRadius: 10,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+      ),
+      child: child,
+    );
+  }
+}
