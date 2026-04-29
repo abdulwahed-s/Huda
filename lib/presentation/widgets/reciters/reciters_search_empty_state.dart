ea53c5eac1e0b9088e031dbadc6f7295894a185e
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-04-29 19:03:13 +0400

    feat: Create RecitersSearchEmptyState widget to display a user-friendly message when no search results are found in the reciters interface

---
+++ lib/presentation/widgets/reciters/reciters_search_empty_state.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:huda/l10n/app_localizations.dart';
+
+class RecitersSearchEmptyState extends StatelessWidget {
+  const RecitersSearchEmptyState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    final theme = Theme.of(context);
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.search_off_rounded,
+              size: 52.sp, color: theme.colorScheme.outline),
+          SizedBox(height: 12.h),
+          Text(
+            l10n.noResultsFound,
+            style: TextStyle(
+                fontSize: 16.sp, color: theme.colorScheme.outline),
+          ),
+        ],
+      ),
+    );
+  }
+}
