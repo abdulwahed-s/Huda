ab18afd110f090a02a46a54b3683f3bf571dce31
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-06-09 19:32:31 +0400

    feat: implement AllAudioLanguagesRepository for fetching and handling audio languages data with error management

---
+++ lib/data/repository/all_audio_languages_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:huda/core/class/dio_errors.dart';
+import 'package:huda/data/api/all_audios_languages_services.dart';
+import 'package:huda/data/models/audio_languages_model.dart';
+
+class AllAudioLanguagesRepository {
+  final AllAudiosLanguagesServices allAudiosLanguagesServices;
+
+  AllAudioLanguagesRepository({required this.allAudiosLanguagesServices});
+
+  Future<List<AudioLanguageModel>> getAllAudioLanguages(String lang) async {
+    try {
+      final response = await allAudiosLanguagesServices.getAudiosLanguages(lang);
+      return response
+          .map((item) => AudioLanguageModel.fromJson(item))
+          .toList();
+    } on DioException catch (e) {
+      throw getDioErrorMessage(e);
+    }
+  }
+}
