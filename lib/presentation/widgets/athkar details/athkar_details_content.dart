74de55f2d19bcd974f6c84597ea0487c0bfa16a7
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-06-17 22:51:10 +0400

    feat: integrate audio service readiness check in AthkarDetailsContent for improved audio playback handling

--- lib/presentation/widgets/athkar details/athkar_details_content.dart
+++ lib/presentation/widgets/athkar details/athkar_details_content.dart
@@ -1,6 +1,7 @@
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:flutter/material.dart';
+import 'package:huda/core/bootstrap/audio_service_ready.dart';
 import 'package:huda/core/services/audio_coordinator.dart';
 import 'package:huda/core/services/service_locator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -264,6 +265,7 @@ class _AthkarDetailsContentState extends State<AthkarDetailsContent>
         }
       } else {
         _coordinator.requestAudio(AudioCoordinator.athkar);
+        await audioServiceReady;
         await _audioPlayer.setAudioSource(AudioSource.uri(
           Uri.parse(audioUrl),
           tag: MediaItem(id: audioUrl, title: 'Athkar'),
