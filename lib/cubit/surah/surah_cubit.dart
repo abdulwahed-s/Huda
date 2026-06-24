c8501c64721627e9bbf3953e3673d70714bb0f19
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-06-24 10:00:00 +0400

    refactor: simplify SurahCubit by removing caching logic and utilizing SurahBuilder for loading Surah data, enhancing code clarity and maintainability

--- lib/cubit/surah/surah_cubit.dart
+++ lib/cubit/surah/surah_cubit.dart
@@ -1,44 +1,18 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/services.dart';
+import 'package:huda/core/quran/surah_builder.dart';
 import 'package:huda/data/models/surah_model.dart';
-import 'dart:convert';
 
 import 'package:meta/meta.dart';
 
 part 'surah_state.dart';
 
 class SurahCubit extends Cubit<SurahState> {
-  static List<dynamic>? _cachedSurahData;
-
   SurahCubit() : super(SurahInitial());
 
   Future<void> loadSurah(int surahNumber) async {
-    // If we already have the specific surah data cached, emit it immediately
-    if (_cachedSurahData != null) {
-      try {
-        final surahJson =
-            _cachedSurahData!.firstWhere((s) => s['number'] == surahNumber);
-        final surah = SurahModel.fromJson(surahJson);
-        emit(SurahLoaded(surah));
-        return;
-      } catch (e) {
-        // If surah not found in cache, proceed with loading
-      }
-    }
-
-    // Only emit loading if we don't have cached data
     emit(SurahLoading());
     try {
-      // Load and cache the data if not already cached
-      if (_cachedSurahData == null) {
-        final String response =
-            await rootBundle.loadString('assets/json/surah_data.json');
-        _cachedSurahData = json.decode(response);
-      }
-
-      final surahJson =
-          _cachedSurahData!.firstWhere((s) => s['number'] == surahNumber);
-      final surah = SurahModel.fromJson(surahJson);
+      final surah = SurahBuilder.buildSurahModel(surahNumber);
       emit(SurahLoaded(surah));
     } catch (e) {
       emit(SurahError("Failed to load surah: $e"));
@@ -46,17 +20,8 @@ class SurahCubit extends Cubit<SurahState> {
   }
 
   static Future<void> preloadSurahData() async {
-    if (_cachedSurahData == null) {
-      try {
-        final String response =
-            await rootBundle.loadString('assets/json/surah_data.json');
-        _cachedSurahData = json.decode(response);
-      } catch (e) {
-        //  Add error handling here if needed
-      }
-    }
+    SurahBuilder.buildAllSurahAyahs();
   }
 
-  // Method to check if data is preloaded
-  static bool get isDataPreloaded => _cachedSurahData != null;
+  static bool get isDataPreloaded => SurahBuilder.isDataPreloaded;
 }
