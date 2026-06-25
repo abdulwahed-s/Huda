2a020bb65848dd686ba056a1026b9bc825da6cec
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-06-25 18:32:12 +0400

    refactor: replace RateMyApp integration with AppReviewService for improved rating management and user feedback collection

--- lib/cubit/rating/rating_cubit.dart
+++ lib/cubit/rating/rating_cubit.dart
@@ -1,84 +1,40 @@
-import 'dart:io';
-
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:rate_my_app/rate_my_app.dart';
+import 'package:flutter/foundation.dart';
+import 'package:huda/core/services/app_review_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 part 'rating_state.dart';
 
 class RatingCubit extends Cubit<RatingState> {
-  late RateMyApp _rateMyApp;
-
-  RatingCubit() : super(RatingInitial()) {
-    _initializeRateMyApp();
-  }
-
-  void _initializeRateMyApp() {
-    _rateMyApp = RateMyApp(
-      preferencesPrefix: 'rateMyApp_',
-      minDays: 3,
-      minLaunches: 7,
-      remindDays: 5,
-      remindLaunches: 10,
-      googlePlayIdentifier: 'com.aw.huda',
-      appStoreIdentifier: '6757343816',
-    );
-  }
+  RatingCubit() : super(RatingInitial());
 
   Future<void> checkIfShouldShowDialog() async {
     emit(RatingLoading());
 
     try {
-      await _rateMyApp.init();
-
-      if (_rateMyApp.shouldOpenDialog) {
-        emit(RatingReady(shouldShow: true));
-      } else {
-        emit(RatingReady(shouldShow: false));
-      }
+      final shouldShow = await AppReviewService.recordLaunchAndEvaluate();
+      emit(RatingReady(shouldShow: shouldShow));
     } catch (e) {
       emit(RatingFailure(message: 'Failed to check rating status'));
     }
   }
-
-  static const String _microsoftStoreProductId = '9p68h8m1g92b';
 
   Future<void> handleRating(int rating, {String? comment, String? contactEmail}) async {
     emit(RatingSubmitting());
 
     try {
       if (rating >= 4) {
-        await _launchStoreForRating();
-        await _rateMyApp.callEvent(RateMyAppEventType.rateButtonPressed);
+        await AppReviewService.launchReviewPage();
+        await AppReviewService.recordDoNotAskAgain();
         emit(RatingSubmitted(rating: rating, message: 'Redirected to store'));
       } else {
         await _submitFeedback(rating, comment ?? '', contactEmail: contactEmail);
-        await _rateMyApp.callEvent(RateMyAppEventType.laterButtonPressed);
+        await AppReviewService.recordRemindLater();
         emit(RatingSubmitted(rating: rating, message: 'Feedback collected'));
       }
     } catch (e) {
       emit(RatingFailure(message: 'Failed to handle rating'));
-    }
-  }
-
-  Future<void> _launchStoreForRating() async {
-    if (Platform.isWindows) {
-      final storeUrl = Uri.parse(
-        'ms-windows-store://review/?ProductId=$_microsoftStoreProductId',
-      );
-      if (await canLaunchUrl(storeUrl)) {
-        await launchUrl(storeUrl);
-      } else {
-        final webUrl = Uri.parse(
-          'https://apps.microsoft.com/detail/$_microsoftStoreProductId',
-        );
-        await launchUrl(webUrl, mode: LaunchMode.externalApplication);
-      }
-    } else {
-      await _rateMyApp.launchStore();
     }
   }
 
@@ -148,14 +104,12 @@ class RatingCubit extends Cubit<RatingState> {
   }
 
   Future<void> callLater() async {
-    await _rateMyApp.callEvent(RateMyAppEventType.laterButtonPressed);
+    await AppReviewService.recordRemindLater();
     emit(RatingReady(shouldShow: false));
   }
 
   Future<void> callNever() async {
-    await _rateMyApp.callEvent(RateMyAppEventType.noButtonPressed);
+    await AppReviewService.recordDoNotAskAgain();
     emit(RatingReady(shouldShow: false));
   }
-
-  RateMyApp get rateMyApp => _rateMyApp;
 }
