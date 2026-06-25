3160e92bc0f9f63bb0ed5c8e7ed3aaa1f4fa54ca
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-06-26 02:39:06 +0400

    refactor: update connectivity check in RecitersScreen to use NetworkInfo for improved reliability

--- lib/presentation/screens/reciters_screen.dart
+++ lib/presentation/screens/reciters_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:huda/core/connection/network_info.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -80,10 +80,10 @@ class _RecitersScreenState extends State<RecitersScreen>
   }
 
   Future<void> _checkConnectivity() async {
-    final result = await Connectivity().checkConnectivity();
+    final isConnected = await NetworkInfo.checkInternetConnectivity();
     if (mounted) {
       setState(() {
-        isOffline = result.contains(ConnectivityResult.none);
+        isOffline = !isConnected;
       });
     }
   }
