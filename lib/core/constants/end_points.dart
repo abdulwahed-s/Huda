61059caaf63792822c66b5e42bcc0fbf7ede8959
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-07-13 10:00:00 +0400

    refactor: clean and format end_points.dart

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -2,7 +2,8 @@ class EndPoints {
   static const String baseUrl = 'https://api.alquran.cloud/v1';
   static const String edition = '/edition';
   static String surahEdition(String identifier) => '/quran/$identifier';
-  static String oneSurahEdition(String identifier, int number) =>'/surah/$number/$identifier';
+  static String oneSurahEdition(String identifier, int number) =>
+      '/surah/$number/$identifier';
   static const String baseUrlAthkar = 'http://www.hisnmuslim.com/api';
   static const arAthkar = "/ar/husn_ar.json";
   static const enAthkar = "/en/husn_en.json";
@@ -10,13 +11,22 @@ class EndPoints {
   static const String hadithBaseUrl = 'https://api.sunnah.com/v1';
   static const String hadithBooks = '/collections';
   static String bookChapter(String bookSlug) => '$hadithBooks/$bookSlug/books';
-  static String hadithDetail(String bookName, String chapterNumber) => '$hadithBooks/$bookName/books/$chapterNumber/hadiths';
-  static const String islamhouseBaseUrl = 'https://api3.islamhouse.com/v3/paV29H2gm56kvLPy';
-  static String books(String lang,int page,String respLang) => '$islamhouseBaseUrl/main/books/$respLang/$lang/$page/25/json';
-  static String bookDetail(String lang, int bookId) => '$islamhouseBaseUrl/main/get-item/$bookId/$lang/json';
-  static String bookLanguages(int sourceId,String respLang) => '$islamhouseBaseUrl/main/get-item-translations/$sourceId/$respLang/json';
-  static String allBooksLanguages(String lang) => '$islamhouseBaseUrl/main/get-available-languages/books/$lang/json';
-  static String audios(String lang, int page, String respLang) => '$islamhouseBaseUrl/main/audios/$respLang/$lang/$page/25/json';
-  static String audioDetail(String lang, int audioId) => '$islamhouseBaseUrl/main/get-item/$audioId/$lang/json';
-  static String allAudiosLanguages(String lang) => '$islamhouseBaseUrl/main/get-available-languages/audios/$lang/json';
+  static String hadithDetail(String bookName, String chapterNumber) =>
+      '$hadithBooks/$bookName/books/$chapterNumber/hadiths';
+  static const String islamhouseBaseUrl =
+      'https://api3.islamhouse.com/v3/paV29H2gm56kvLPy';
+  static String books(String lang, int page, String respLang) =>
+      '$islamhouseBaseUrl/main/books/$respLang/$lang/$page/25/json';
+  static String bookDetail(String lang, int bookId) =>
+      '$islamhouseBaseUrl/main/get-item/$bookId/$lang/json';
+  static String bookLanguages(int sourceId, String respLang) =>
+      '$islamhouseBaseUrl/main/get-item-translations/$sourceId/$respLang/json';
+  static String allBooksLanguages(String lang) =>
+      '$islamhouseBaseUrl/main/get-available-languages/books/$lang/json';
+  static String audios(String lang, int page, String respLang) =>
+      '$islamhouseBaseUrl/main/audios/$respLang/$lang/$page/25/json';
+  static String audioDetail(String lang, int audioId) =>
+      '$islamhouseBaseUrl/main/get-item/$audioId/$lang/json';
+  static String allAudiosLanguages(String lang) =>
+      '$islamhouseBaseUrl/main/get-available-languages/audios/$lang/json';
 }
