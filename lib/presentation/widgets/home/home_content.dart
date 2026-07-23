ca1dc4d332cabc3e7c039a945536a5257b9f2de0
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-07-23 10:00:00 +0400

    feat: render configurable home themes and sections

--- lib/presentation/widgets/home/home_content.dart
+++ lib/presentation/widgets/home/home_content.dart
@@ -1,28 +1,16 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:huda/core/utils/platform_utils.dart';
-import 'package:huda/core/utils/responsive_utils.dart';
-import 'package:huda/l10n/app_localizations.dart';
-import 'package:huda/presentation/widgets/home/feature_grid.dart';
-import 'package:huda/presentation/widgets/home/quran_feature_stack_card.dart';
-import 'package:huda/presentation/widgets/home/special_event_card.dart';
-import 'package:huda/presentation/widgets/home/special_event_dialog.dart';
-import 'package:huda/cubit/islamic_event/islamic_event_cubit.dart';
-import 'package:huda/cubit/quran_radio/quran_radio_cubit.dart';
 import 'package:huda/core/routes/app_route.dart';
+import 'package:huda/cubit/home_customization/home_customization_cubit.dart';
+import 'package:huda/cubit/home_customization/home_customization_state.dart';
+import 'package:huda/cubit/quran_radio/quran_radio_cubit.dart';
+import 'package:huda/data/models/home/home_preferences.dart';
+import 'package:huda/presentation/widgets/home/catalog/home_feature_catalog.dart';
+import 'package:huda/presentation/widgets/home/home_theme_renderer.dart';
+import 'package:huda/presentation/widgets/home/shared/home_section_widgets.dart';
 
 class HomeContent extends StatelessWidget {
-  final AnimationController animationController;
-  final Animation<double> fadeAnimation;
-  final Animation<Offset> slideAnimation;
-  final VoidCallback refreshHomeData;
-  final Function(Map<String, dynamic>) openLastReadSurah;
-  final Function(dynamic) openLastReciterAudio;
-  final Function(dynamic) openLastRadioStation;
-  final bool isDarkMode;
-
   const HomeContent({
     super.key,
     required this.animationController,
@@ -32,181 +20,109 @@ class HomeContent extends StatelessWidget {
     required this.openLastReadSurah,
     required this.openLastReciterAudio,
     required this.openLastRadioStation,
+    required this.openSurah,
+    required this.onCustomize,
     required this.isDarkMode,
   });
 
-  List<FeatureItem> _getFeatures(BuildContext context) {
-    return [
-      FeatureItem(
-        title: AppLocalizations.of(context)!.prayerTimes,
-        svgAsset: 'assets/images/prayertimeicon.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.prayerTimes),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.hadith,
-        svgAsset: 'assets/images/hadithsicon.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.hadith),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.athkar,
-        svgAsset: 'assets/images/athkaricon.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.athkar),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.hijriCalendar,
-        svgAsset: 'assets/images/hijricalendaricon.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.hijriCalendar),
-      ),
-      if (PlatformUtils.isMobile)
-        FeatureItem(
-          title: AppLocalizations.of(context)!.miqaatLock,
-          svgAsset: 'assets/images/miqaatlock.svg.vec',
-          onTap: () => Navigator.pushNamed(context, AppRoute.miqaatLock),
-        ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.books,
-        svgAsset: 'assets/images/booksicon.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.books),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.audios,
-        icon: Icons.headphones_rounded,
-        svgAsset: "assets/images/audio.svg.vec",
-        onTap: () => Navigator.pushNamed(context, AppRoute.audios),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.hudaAI,
-        icon: Icons.auto_awesome,
-        onTap: () => Navigator.pushNamed(context, AppRoute.hudaAI),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.islamicChecklist,
-        icon: Icons.checklist,
-        onTap: () => Navigator.pushNamed(context, AppRoute.islamicChecklist),
-      ),
-      if (PlatformUtils.isMobile)
-        FeatureItem(
-          title: AppLocalizations.of(context)!.qiblahDirection,
-          svgAsset: 'assets/images/qiblahicon.svg.vec',
-          onTap: () => Navigator.pushNamed(context, AppRoute.qiblah),
-        ),
-      if (!kIsWeb && !PlatformUtils.isLinux)
-        FeatureItem(
-          title: AppLocalizations.of(context)!.notifications,
-          icon: Icons.notifications,
-          onTap: () => Navigator.pushNamed(context, AppRoute.notification),
-        ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.ramadan,
-        svgAsset: 'assets/images/ramadan.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.ramadan),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.zakatCalculator,
-        svgAsset: 'assets/images/zakat.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.zakatCalculator),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.tasbih,
-        svgAsset: 'assets/images/tasbihicon.svg.vec',
-        onTap: () => Navigator.pushNamed(context, AppRoute.tasbih),
-      ),
-      FeatureItem(
-        title: AppLocalizations.of(context)!.settings,
-        icon: Icons.settings,
-        onTap: () => Navigator.pushNamed(context, AppRoute.settings),
-      ),
-      if (PlatformUtils.isMobile)
-        FeatureItem(
-          title: AppLocalizations.of(context)!.widgetManagement,
-          icon: Icons.widgets,
-          onTap: () => Navigator.pushNamed(context, AppRoute.widgetManagement),
-        ),
-    ];
-  }
+  final AnimationController animationController;
+  final Animation<double> fadeAnimation;
+  final Animation<Offset> slideAnimation;
+  final VoidCallback refreshHomeData;
+  final Function(Map<String, dynamic>) openLastReadSurah;
+  final Function(dynamic) openLastReciterAudio;
+  final Function(dynamic) openLastRadioStation;
+  final void Function(int surah, int ayah) openSurah;
+  final VoidCallback onCustomize;
+  final bool isDarkMode;
 
   @override
   Widget build(BuildContext context) {
+    Widget defaultEntrance(Widget child) {
+      return FadeTransition(
+        opacity: fadeAnimation,
+        child: SlideTransition(position: slideAnimation, child: child),
+      );
+    }
+
     return SliverToBoxAdapter(
-      child: AnimatedBuilder(
-        animation: animationController,
-        builder: (context, child) {
-          return FadeTransition(
-            opacity: fadeAnimation,
-            child: SlideTransition(
-              position: slideAnimation,
-              child: Padding(
+      child: BlocBuilder<HomeCustomizationCubit, HomeCustomizationState>(
+        builder: (context, customizationState) {
+          if (customizationState is! HomeCustomizationReady) {
+            return defaultEntrance(
+              Padding(
                 padding: EdgeInsets.all(20.w),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    BlocBuilder<IslamicEventCubit, IslamicEventState>(
-                      builder: (context, eventState) {
-                        if (eventState is! IslamicEventActive) {
-                          return const SizedBox.shrink();
-                        }
-                        return Padding(
-                          padding: EdgeInsets.only(
-                            bottom: context.responsive(
-                              mobile: 16.h,
-                              tablet: 20.h,
-                              desktop: 24.h,
-                            ),
-                          ),
-                          child: SpecialEventCard(
-                            event: eventState.event,
-                            isDarkMode: isDarkMode,
-                            onTap: () => showSpecialEventDialog(
-                              context,
-                              eventState.event.eventKey,
-                              isDarkMode,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    FeatureGrid(
-                      isDarkMode: isDarkMode,
-                      features: _getFeatures(context),
-                      openLastReadSurah: openLastReadSurah,
-                      openLastReciterAudio: openLastReciterAudio,
-                      openLastRadioStation: openLastRadioStation,
-                      quranStackCard: QuranFeatureStackCard(
-                        isDarkMode: isDarkMode,
-                        index: 0,
-                        stackLabel: AppLocalizations.of(context)!.quranKit,
-                        quranLabel: AppLocalizations.of(context)!.quran,
-                        audioLabel: AppLocalizations.of(context)!.quranAudio,
-                        radioLabel: AppLocalizations.of(context)!.quranRadio,
-                        bookmarkLabel: AppLocalizations.of(context)!.bookmarks,
-                        onQuranTap: () async {
-                          await Navigator.pushNamed(context, '/homeQuran');
-                          refreshHomeData();
-                        },
-                        onAudioTap: () =>
-                            Navigator.pushNamed(context, AppRoute.quranAudio),
-                        onRadioTap: () async {
-                          await Navigator.pushNamed(
-                              context, AppRoute.quranRadio);
-                          if (context.mounted) {
-                            await context
-                                .read<QuranRadioCubit>()
-                                .saveCurrentStation();
-                          }
-                          refreshHomeData();
-                        },
-                        onBookmarkTap: () =>
-                            Navigator.pushNamed(context, AppRoute.bookmarks),
-                        openLastReciterAudio: openLastReciterAudio,
-                        openLastRadioStation: openLastRadioStation,
-                      ),
-                    ),
-                  ],
-                ),
+                child: const Center(child: CircularProgressIndicator()),
               ),
+            );
+          }
+
+          final actions = HomeDashboardActions(
+            openQuran: () async {
+              await Navigator.pushNamed(context, AppRoute.homeQuran);
+              refreshHomeData();
+            },
+            openPrayerTimes: () =>
+                Navigator.pushNamed(context, AppRoute.prayerTimes),
+            openAudio: () => Navigator.pushNamed(context, AppRoute.quranAudio),
+            openRadio: () async {
+              await Navigator.pushNamed(context, AppRoute.quranRadio);
+              if (context.mounted) {
+                await context.read<QuranRadioCubit>().saveCurrentStation();
+                refreshHomeData();
+              }
+            },
+            openBookmarks: () =>
+                Navigator.pushNamed(context, AppRoute.bookmarks),
+            openSurah: openSurah,
+          );
+          final featureActions = HomeFeatureActions(
+            openQuran: actions.openQuran,
+            openQuranKit: () => _showQuranKit(context, actions),
+          );
+          final preferences = customizationState.preferences;
+          final prayerToday =
+              preferences.selectedTheme == HomeThemeId.prayerToday;
+          final quranJourney =
+              preferences.selectedTheme == HomeThemeId.quranJourney;
+          final focusedTheme = preferences.selectedTheme != HomeThemeId.classic;
+          final content = Padding(
+            padding: prayerToday || quranJourney
+                ? EdgeInsets.only(bottom: 20.h)
+                : EdgeInsets.all(20.w),
+            child: HomeThemeRenderer(
+              theme: preferences.selectedTheme,
+              configuration:
+                  preferences.configurationFor(preferences.selectedTheme),
+              features: HomeFeatureCatalog.available(context, featureActions),
+              actions: actions,
+              isDark: isDarkMode,
+              openLastReadSurah: openLastReadSurah,
+              openLastReciterAudio: openLastReciterAudio,
+              openLastRadioStation: openLastRadioStation,
+              onRetry: refreshHomeData,
+              onCustomize: onCustomize,
             ),
           );
+          return focusedTheme ? content : defaultEntrance(content);
         },
+      ),
+    );
+  }
+
+  void _showQuranKit(BuildContext context, HomeDashboardActions actions) {
+    showModalBottomSheet<void>(
+      context: context,
+      showDragHandle: true,
+      isScrollControlled: true,
+      builder: (sheetContext) => SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20.w, 0, 20.w, 24.h),
+          child: HomeQuranToolsSection(
+            isDark: Theme.of(sheetContext).brightness == Brightness.dark,
+            actions: actions,
+          ),
+        ),
       ),
     );
   }
