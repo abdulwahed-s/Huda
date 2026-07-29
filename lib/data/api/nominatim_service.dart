15101b562b60e37d9c46f298b95613f8c44767c5
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2026-07-29 10:00:00 +0400

    feat(location): support localized places autocomplete

--- lib/data/api/nominatim_service.dart
+++ lib/data/api/nominatim_service.dart
@@ -1,18 +1,57 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:huda/data/models/location_search_suggestion.dart';
 import 'package:huda/data/models/placemark_model.dart';
 
 class NominatimService {
   final _functions = Supabase.instance.client.functions;
 
-  Future<PlacemarkModel> getPlacemark(double lat, double lon) async {
-    final res = await _functions
-        .invoke('geocode-proxy', body: {'lat': lat, 'lon': lon});
+  Future<PlacemarkModel> getPlacemark(
+    double lat,
+    double lon, {
+    String languageCode = 'en',
+  }) async {
+    final res = await _functions.invoke(
+      'geocode-proxy',
+      body: {'lat': lat, 'lon': lon, 'language': languageCode},
+    );
     return PlacemarkModel.fromJson(res.data);
   }
 
-  Future<PlacemarkModel> searchLocation(String address) async {
-    final res =
-        await _functions.invoke('geocode-proxy', body: {'address': address});
+  Future<List<LocationSearchSuggestion>> autocompleteCities(
+    String query, {
+    required String languageCode,
+  }) async {
+    final res = await _functions.invoke(
+      'geocode-proxy',
+      body: {
+        'action': 'autocomplete',
+        'query': query,
+        'language': languageCode,
+      },
+    );
+
+    final data = res.data;
+    if (data is! Map) return const [];
+
+    final rawSuggestions = data['suggestions'];
+    if (rawSuggestions is! List) return const [];
+
+    return rawSuggestions
+        .whereType<Map>()
+        .map((item) =>
+            LocationSearchSuggestion.fromJson(Map<String, dynamic>.from(item)))
+        .where((suggestion) => suggestion.isValid)
+        .toList();
+  }
+
+  Future<PlacemarkModel> resolvePlace(
+    String placeId, {
+    required String languageCode,
+  }) async {
+    final res = await _functions.invoke(
+      'geocode-proxy',
+      body: {'placeId': placeId, 'language': languageCode},
+    );
     return PlacemarkModel.fromJson(res.data);
   }
 }
